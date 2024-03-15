e49bfcd30a4503cf4236f0a2eb70ae28142f8582
MattieDevon <matijar01@protonmail.com>
2024-03-15 01:08:39 +0100

    lab3

---
+++ lab3/komendy.sh
@@ -0,0 +1,22 @@
+git commit
+git commit --amend
+git branch 
+git checkout [branch]
+git switch
+git checkout [commit]
+git merge
+git rebase
+git checkout HEAD^
+git checkout HEAD~
+git branch -f
+git reset
+git revert
+git cherry-pick
+git rebase -i
+git tag
+git describe
+git clone
+git fetch
+git pull
+git pull --rebase
+git push
