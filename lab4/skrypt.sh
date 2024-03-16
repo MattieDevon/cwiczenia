3577c9cf79f0c489b003a994c2c9924474839782
MattieDevon <matijar01@protonmail.com>
2024-03-17 00:55:58 +0100

    zadanie wykonane

--- lab4/skrypt.sh
+++ lab4/skrypt.sh
@@ -4,12 +4,18 @@
 function log(){
 	for ((i=1;i<=$1;i++))
 	do
-	printf "Nazwa skryptu: $0\nLog Numer: $i\n$(date)" > log$i.txt
+	printf "Nazwa skryptu: $(basename "$0")\nLog Numer: $i\n$(date)" > log$i.txt
+	done
+}
+function error(){
+	mkdir -p error
+	for ((i=1;i<=$1;i++))
+	do
+	echo "Error" > error/error$i.txt
 	done
 }
 
-
-flagi=$(getopt -o hdl: --long help,date,log:,init -- "$@")
+flagi=$(getopt -o hdl:e: --long help,date,log:,init,error: -- "$@")
 
 eval set -- "$flagi"
 while [ : ]; do
@@ -23,13 +29,15 @@ while [ : ]; do
         shift
         ;;
     -l | --log)
-		
         log "$2"
+        shift 2
+        ;;
+    -e | --error)
+        error "$2"
         shift 2
         ;;
 	--init)
 		cp "$BASH_SOURCE" "$(pwd)/script.sh"
-		
 		shift
 		;;
     --) shift; 
@@ -37,3 +45,9 @@ while [ : ]; do
         ;;
   esac
 done
+
+
+echo "The script you are running has:"
+echo "basename: [$(basename "$0")]"
+echo "dirname : [$(dirname "$0")]"
+echo "pwd     : [$(pwd)]"
