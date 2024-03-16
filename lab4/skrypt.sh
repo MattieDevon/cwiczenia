7c59bfa2a17b65e6f7d0f15465cb354c905177e8
MattieDevon <matijar01@protonmail.com>
2024-03-17 00:58:20 +0100

    fix

--- lab4/skrypt.sh
+++ lab4/skrypt.sh
@@ -45,9 +45,3 @@ while [ : ]; do
         ;;
   esac
 done
-
-
-echo "The script you are running has:"
-echo "basename: [$(basename "$0")]"
-echo "dirname : [$(dirname "$0")]"
-echo "pwd     : [$(pwd)]"
